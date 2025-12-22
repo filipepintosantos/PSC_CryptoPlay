553bd1bc82af1e7f8a7b67854b92a271baf16960
filipepintosantos <filipepintosantos@gmail.com>
2025-12-22 22:48:20 +0000

    V 4.2.3 - got help from Copilot 3

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 PSC CryptoPlay - Cryptocurrency Price Tracking and Analysis Tool
 """
 
-__version__ = "4.2.2"
+__version__ = "4.2.3"
 __date__ = "2025-12-22"
 __author__ = "Pinto Santos Consulting"
  
