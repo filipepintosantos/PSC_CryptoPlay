72c84d717a982de6ab32d6d70f9b593411603c2e
filipepintosantos <filipepintosantos@gmail.com>
2025-12-29 12:12:13 +0000

    chore(clean): remove archived stubs and .orig files

--- scripts/recreate_price_quotes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""Archived migration: recreated to legacy folder.
-
-Original file moved to `scripts/legacy/recreate_price_quotes.py.orig`.
-"""
-import sys
-
-print("This migration has been archived to scripts/legacy/recreate_price_quotes.py.orig and is no longer active.")
-sys.exit(0)
